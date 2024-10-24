6fb99940b677928a02ab5250ccb35861fdf6ca1d
dtoecho <dtoecho@gmail.com>
2024-10-24 17:15:12 +0900

    분기배포

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,16 @@
 const nextConfig = {
   reactStrictMode: true,
   trailingSlash: true,
+  generateBuildId: () => "codecamp-deploy-project",
+
+  //아래 주소들만 out 폴더에 만들어줘! (getServerSideProps 있는 페이지는 제외시키면 되겠지.)
+  exportPathMap: () => {
+    return {
+      "/": { page: "/" },
+      "/boards": { page: "/boards" },
+      "/404": { page: "/404" },
+    };
+  },
 };
 
 export default nextConfig;
